13273d45816c1cd541c1b1485b95b66f7e065209
Sneha <snehalall.sneha@gmail.com>
2023-01-13 15:54:59 +0530

    feat:add to use if-else statements

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+n='orange'
+n=n.length
+
+
+
+if (n > 5) {  
+    console.log("The fruit name has more than five characters." )  
+  } else {  
+    console.log("The fruit name has five characters or less.")  
+  }  
