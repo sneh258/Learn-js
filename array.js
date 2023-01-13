d5d3ef48fa5c155a3919d26b19924d646ba3b8d6
Sneha <snehalall.sneha@gmail.com>
2023-01-14 01:52:29 +0530

    feat: add description to use array

---
+++ array.js
@@ -0,0 +1,9 @@
+pizzaToppings =['tomato sauce', 'cheese','pepperoni']  
+  
+
+// for(let i=0;i<pizzaToppings.length;i++)
+// {
+//     console.log(pizzaToppings[i])
+// }
+
+console.log(pizzaToppings)
